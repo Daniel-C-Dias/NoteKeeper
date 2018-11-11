89b51f1e402d5c537b45d9be53ec5212e9d1a88f
Daniel-C-Dias <danielcanastro@gmail.com>
2018-11-11 19:18:41 +0000

    Completed Tutorial Working with Android Tools and Testing

--- app/src/main/java/daniel/dias/notekeeper/NoteActivity.java
+++ app/src/main/java/daniel/dias/notekeeper/NoteActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -13,7 +14,7 @@ import android.widget.Spinner;
 import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
-
+    private final String TAG = getClass().getSimpleName();
     public static final String NOTE_POSITION = "daniel.dias.notekeeper.NOTE_POSITION";
     public static final String ORIGINAL_NOTE_COURSE_ID = "daniel.dias.notekeeper.ORIGINAL_NOTE_COURSE_ID";
     public static final String ORIGINAL_NOTE_TITLE = "daniel.dias.notekeeper.ORIGINAL_NOTE_TITLE";
@@ -58,6 +59,8 @@ public class NoteActivity extends AppCompatActivity {
 
         if(!mIsNewNote)
             displayNote(mSpinnerCourses, mTextNoteTitle, mTextNoteText);
+
+        Log.d(TAG, "onCreate");
     }
 
     private void restoreOriginalNoteValues(Bundle savedInstanceState) {
@@ -111,6 +114,7 @@ public class NoteActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         if(mIsCancelling){
+            Log.i(TAG, "Cancelling note at position " + mNotePosition);
             if(mIsNewNote){
                 DataManager.getInstance().removeNote(mNotePosition);
             }
@@ -121,6 +125,8 @@ public class NoteActivity extends AppCompatActivity {
         else {
             saveNote();
         }
+
+        Log.d(TAG, "onPause");
     }
 
     @Override
